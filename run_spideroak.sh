e5fccbec01ac3f3a78ea58fa890c178a39065c6f
Todd Eddy <vr@vrillusions.com>
2015-05-03 02:23:56 -0400

    Cleanup run_spideroak script
    * can now pass options to command to set things, see
      `run_spideroak.sh -h`
    * increased purge days default to 90 days
    * use a directory for a flock file which is more portable
    * remove specifying path to SpiderOak and just verify it's in $PATH
    * add a `-q` option to silence all output aside from errors which always
      show.

--- run_spideroak.sh
+++ run_spideroak.sh
@@ -10,72 +10,123 @@
 # 0   - no errors
 # 1   - some unhandled error (this script won't using exit code of 1)
 # 100 - script lockfile exists so not running
-# 101 - SpiderOak is already running (SpiderOak checks this as well but it
+# 101 - the `SpiderOak` command doesn't exist in $PATH
+# 102 - SpiderOak is already running (SpiderOak checks this as well but it
 #       still (wrongfully IMO) returns an exit code of 0 so can't rely on it)
 
 set -e
 set -u
 
-# Command that is run, default should be fine
-spideroak_cmd="/usr/bin/SpiderOak"
 
-# The output from your .config/SpiderOak directory should be sufficient but you
-# may also log the output from this script by having this point to some file
-# instead of just /dev/null
-logfile="/dev/null"
-
-# This will save the output of `SpiderOak --selection` to this file. Default
-# assumes SpiderOak is running as root
-selection_backup="/root/spideroak_selection.txt"
-
-# Remove items from SpiderOak that have delete from system after this many days.
-purge_days=30
-
-# Shouldn't have to change this unless for some reason you can't create files in
-# /var/lock
-lockfile="/var/lock/run_spideroak.lock"
+# -- script constants --
+# What directory this will use for a lock file
+_lockfile="/var/lock/run_spideroak.lock"
 
 
+# -- logging functions --
 # Usage: log "whatever you want to log"
 log () {
-    printf "%b\n" "$(date +"%Y-%m-%dT%H:%M:%S%z") $*"
+    if [[ "${quiet:-false}" == "false" ]]; then
+        printf "%b\n" "$(date +"%Y-%m-%dT%H:%M:%S%z") $*"
+    fi
 }
 
 
-# If the lockfile exists already, then this must be running already and so exit
-[[ -f "${lockfile}" ]] && exit 100
-# trap should cleanup lock file no matter how script exits
-trap "rm -f \"${lockfile}\"" EXIT
-touch "${lockfile}"
+# -- create a flock dir --
+__sig_exit () {
+    rmdir "${_lockfile}"
+}
+if ! mkdir "${_lockfile}" 2>/dev/null ; then
+    echo "unable to create lock file, exiting" >&2
+    exit 100
+fi
+trap __sig_exit EXIT
+
+
+# -- option handling --
+# defaults
+verbose_logfile='/dev/null'
+quiet='false'
+selection_file='/root/spideroak-selection.txt'
+purge_days=90
+
+while getopts ":hp:qs:v:" opt; do
+    case ${opt} in
+    h)
+        echo "Usage: $(basename $0) [OPTION]"
+        echo 'Run spideroak and maintenance tasks'
+        echo
+        echo 'Options:'
+        echo '  -h  this help message'
+        echo "  -p  purge files deleted more than this many days (default: ${purge_days})"
+        echo '  -q  suppress all output other than errors'
+        echo "  -s  write current selection to this file (default: ${selection_file})"
+        echo "  -v  verbose log file (default: ${verbose_logfile})"
+        exit 0
+        ;;
+    p)
+        purge_days=${OPTARG}
+        ;;
+    q)
+        quiet='true'
+        ;;
+    s)
+        selection_file=${OPTARG}
+        ;;
+    v)
+        verbose_logfile=${OPTARG}
+        ;;
+    \?)
+        echo "Invalid option: -${OPTARG}" >&2
+        exit 1
+        ;;
+    :)
+        echo "Option -${OPTARG} requires an argument" >&2
+        exit 1
+        ;;
+    esac
+done
+shift $(expr ${OPTIND} - 1)
+
+
+# Make sure SpiderOak is in our path
+if ! command -v SpiderOak 1>/dev/null 2>&1; then
+    echo 'SpiderOak is not installed or not in path' >&2
+    exit 101
+fi
 
 
 # Check if SpiderOak is already running and cancel if it is
 spideroak_running="$(pgrep SpiderOak &>/dev/null; echo $?)"
 if [[ "${spideroak_running}" -eq "0" ]]; then
-    echo "SpiderOak is already running, not rerunning" 1>&2
-    exit 101
+    echo "SpiderOak is already running, not rerunning" >&2
+    exit 102
 fi
 
 
 # Get a list of selections since you can't easily pull this from SpiderOak
 log "Save backup selection to file"
-"${spideroak_cmd}" --selection >"${selection_backup}"
+SpiderOak --selection >"${selection_file}"
 
-# Purposely made it overwrite the log or else it would get too large. could
-# cause an issue if this runs multiple times though.
+
+# Intentionally overwrites logfile to prevent it from filling disk
 log "Running SpiderOak"
-"${spideroak_cmd}" --verbose --batchmode >"${logfile}"
+SpiderOak --verbose --batchmode >"${verbose_logfile}"
+
 
 # Does default schedule:
 #   - hourly for last 24 hours
 #   - daily for last month
 #   - weekly thereafter
 log "Purge historical versions"
-"${spideroak_cmd}" --verbose --purge-historical-versions >>"${logfile}"
+SpiderOak --verbose --purge-historical-versions >>"${verbose_logfile}"
+
 
 # Remove items from trash. To keep forever comment or remove line
 log "Purge deleted items"
-"${spideroak_cmd}" --verbose --purge-deleted-items=${purge_days} >>"${logfile}"
+SpiderOak --verbose --purge-deleted-items=${purge_days} >>"${verbose_logfile}"
+
 
 log "Finished successfully"
+
 exit 0
